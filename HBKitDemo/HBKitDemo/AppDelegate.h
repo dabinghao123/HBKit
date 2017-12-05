b44e390d9394388b012626339492b4f094dd9ff1
huangbing <hb@shikee.tv>
2017-12-05 14:34:57 +0800

    还原

---
+++ HBKitDemo/HBKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HBKitDemo
+//
+//  Created by huangbing on 2017/12/5.
+//  Copyright © 2017年 uplayTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
