2eb8e8247e274e4f9686cf9885cef0ca8f26b8dc
huangbing <hb@shikee.tv>
2017-12-05 14:28:22 +0800

    初始化项目

---
+++ HBKitDemo/HBKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HBKitDemo
+//
+//  Created by huangbing on 2017/12/5.
+//  Copyright © 2017年 uplayTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HBKitDemo/HBKitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HBKitDemo
+//
+//  Created by huangbing on 2017/12/5.
+//  Copyright © 2017年 uplayTeam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
